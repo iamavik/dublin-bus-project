243f27103410d19c062225b615395211ea6a80da
iamavik <47152762+iamavik@users.noreply.github.com>
2019-07-22 15:39:07 +0530

    Added a separate functional route for tourist modes
    The route name is 'my-tourist-places/'

--- django_test/bus/urls.py
+++ django_test/bus/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('my-ajax-test/', views.findroutedetails, name='findroutedetails'),
     path('about/',views.about,name='bus_test-about'),
     path('my-weather-data/', views.getweatherdetails, name='getweatherdetails'),
-    path('my-nearest-bus-stop/',views.getnearestbusstops,name = 'getnearestbusstops'),
+    path('my-nearest-bus-stop/',views.getnearestbusstops,name = 'getnearestbusstops'),  
+    path('my-tourist-places/',views.getalltouristplaces,name = 'getalltouristplaces'),
     
     
 ]
