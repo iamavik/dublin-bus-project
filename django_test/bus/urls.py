ce50686437c99dc274a129d53ddaeae1ed746b7b
iamavik <47152762+iamavik@users.noreply.github.com>
2019-07-29 05:17:38 +0530

    Fixed the route issue to 'my-tourist-places' and removed the 'about' route

--- django_test/bus/urls.py
+++ django_test/bus/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='bus_test-home'),
     path('my-ajax-test/', views.findroutedetails, name='findroutedetails'),
-    path('about/',views.about,name='bus_test-about'),
     path('my-weather-data/', views.getweatherdetails, name='getweatherdetails'),
     path('my-nearest-bus-stop/',views.getnearestbusstops,name = 'getnearestbusstops'),  
     path('my-tourist-places/',views.getalltouristplaces,name = 'getalltouristplaces'),
